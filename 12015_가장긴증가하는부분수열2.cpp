103a52fed42413947772f53e6556bed56b92c546
jinhyukoo <faill456@naver.com>
2020-12-29 21:38:40 +0900

    lower_bound를 활용한 LIS 문제

---
+++ 12015_가장긴증가하는부분수열2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(void) {
+  int N;
+  vector<int> L;
+  cin>>N;
+  for(int i = 0 ; i<N ; i++){
+    int input;
+    cin>>input;
+    if(L.empty()){
+      L.push_back(input);
+      continue;
+    }
+    auto lbound = lower_bound(L.begin(), L.end(), input);
+    if(lbound == L.begin() + L.size()) L.push_back(input);
+    else *lbound = input;
+  }
+  cout<<L.size();
+  return 0;
+}
