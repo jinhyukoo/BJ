92c573625d0920e34ddfb412c93dd43784e7454d
jinhyukoo <faill456@naver.com>
2021-01-02 02:25:21 +0900

    자바스크립트 문제풀이

--- 14681_사분면고르기.js
+++ 14681_사분면고르기.js
@@ -15,12 +15,10 @@ rl.on('line', function (line) {
     process.exit();
 });
 
-
-
 const main = () => {
     const x = input[0];
     const y = input[1];
-    
+
     if(x>0 && y>0) console.log(1);
     if(x>0 && y<0) console.log(4);
     if(x<0 && y>0) console.log(2);
